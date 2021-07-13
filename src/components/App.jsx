dd7c5e57c04714cb92c5bcbcde651914e81adc17
pilse <gogosky1175@gmail.com>
2021-07-13 17:30:29 +0900

    feat: github-follower-map

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GithubUser from './GithubUser/GithubUser';
+import './App.css';
+
+function App() {
+  return (
+     <GithubUser/>
+  );
+}
+
+export default App;
